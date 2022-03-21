cc0ee4f4d2ce70217e85e4edcd4be3c304bac751
Khaxdev <98678822+Khaxdev@users.noreply.github.com>
2022-03-21 15:58:45 -0500

    init

---
+++ bio-comments.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: BIO PCC
+ * Description: Plugin control comments
+ * Plugin URI: https://github.com/Khaxdev/BIO-Comments
+ * Author: Khaxdev
+ * Version: 1.0.0
+ * Author URI: https://github.com/Khaxdev/
+ *
+ * Text Domain: BIO PCC
+ *
+ * @package BIO PCC
+ *
+ * FOR CONTROL YOUR COMMENTS AND CREATE A GOOD IMAGE FOR IU
+ * GNU General Public License for more details.
+ */
+
+ 
